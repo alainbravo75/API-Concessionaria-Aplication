868f25414c7df7acb3446d74dae024416a823eb3
alain_bravo@ <83651651+alainbravo75@users.noreply.github.com>
2021-09-25 14:34:33 -0300

    Modelagem do BD

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
-const product = require('./models/product')
+const productCar = require('./models/product')
 
 require('dotenv').config();
+
+const port = 8081;
+
+//chamando o j.son na requisições
+app.use(express.json());
+
 
 const bodyParser = require('body-parser');
 
 const urlDB = process.env.MONGODB_URL
-console.log(urlDB);
 
-const app = express();
 //conectar mongoose com express
-mongoose.connect(urlDB), {
+mongoose.connect('mongodb+srv://projetocarro:carro@cluster0.7ataw.mongodb.net/databaseconcessionaria?retryWrites=true&w=majority'), {
     useUnifieldTopology: true,
     useNewUrlParser: true
 }, console.log('Connected to database');
@@ -21,26 +26,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) =>{
-    res.send('ok')
-
-    product.create({carro: "Chevrolet", modelo: "Onix"})
+   res.send('ok')
+    //productCar.create({carro: 'TOYOTA', descricao: 'Novo carro', modelo: 'Yaris', ano: (2021, 05 , 15), cor: "verde", preco: 23.500, quantidade: 5 })
 });
 
-app.get('/new', (req, res) => {
-    product.insertOne({carro: 'Toyota', modelo:'Hiace'})
+//Cadastrando um produto
+app.get('/product', (req, res) => {
+    res.send('Requisição POST foi chamada')
 });
 
-app.listen(8081, () => {
-    console.log('Server running')
+
+app.post('/product', (req, res) => {
+    const {carro, descricao, modelo, ano, cor, preco, quantidade} = req.body;
+    
+    const car = {
+        carro,
+        descricao,
+        modelo,
+        ano,
+        cor,
+        preco,
+        quantidade
+    }
+    
+    productCar.insertMany(productCar)
+    console.log(req.body)
+   // res.send(product)
+   res.json({car})
+});
+
+//Rota de PUT
+app.get('/product/:productId', (req, res) => {
+    res.send('A requisição PUT foi chamada')
+})
+app.put('/product/:productId', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: "Produto atualizado com sucesso!"})
 })
 
-
-
-
-
-//console.log('ola mundo')
-
-
-
-
-
+app.listen(port, () => {
+    console.log('Server running')
+})
