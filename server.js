d890b01bdc4b6039e235104e53fe90412cb85de4
alain_bravo@ <83651651+alainbravo75@users.noreply.github.com>
2021-09-28 20:13:34 -0300

    ATUALIZAÇÃO DA ROTA UPDATE

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const productCar = require('./models/product')
+const productCar = require('./models/product');
+//const swaggerUi = require('swagger-ui-express');
+//const swaggerDocument = require('./swagger.json');
 
 
 require('dotenv').config();
@@ -10,11 +12,11 @@ const port = 8081;
 
 //chamando o j.son na requisições
 app.use(express.json());
-
-
 const bodyParser = require('body-parser');
-
 const urlDB = process.env.MONGODB_URL
+
+//Documentação
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //conectar mongoose com express
 mongoose.connect('mongodb+srv://projetocarro:carro@cluster0.7ataw.mongodb.net/databaseconcessionaria?retryWrites=true&w=majority'), {
@@ -37,7 +39,7 @@ app.get('/', (req, res) =>{
 //});
 
 //Cadastrando um produto com POST
-app.post('/product', async (req, res) => {
+app.post('/carros', async (req, res) => {
     
     const {carro, modelo, descricao, ano, cor, preco, quantidade, carImage} = req.body;
     const car = {
@@ -60,7 +62,7 @@ app.post('/product', async (req, res) => {
 
         //Listar todos os carros cadastrados com requisição GET
 
-        app.get('/listar', async(req, res) => {
+        app.get('/carros', async(req, res) => {
             try {
                 const listProduct = await productCar.find(req.body)
                 return res.status(200).json(listProduct)
@@ -70,12 +72,12 @@ app.post('/product', async (req, res) => {
         });
 
         //listar um carro específico com id
-        app.get('/get/:id', async(req, res) => {
+        app.get('/carros/:id', async(req, res) => {
             try {
                 const product = await productCar.findById(req.params.id)
                 return res.status(200).json(product)
             } catch(err) {
-                return res.status(400).json(err)
+                return res.status(404).json(err)
             }
         });
     
@@ -83,10 +85,12 @@ app.post('/product', async (req, res) => {
 
         //Método PUT
 
-        app.put('/atualizar/:id', async (req, res) => {
+        app.put('/carros/:id', async (req, res) => {
             try {
-                const updateProduct = await productCar.updateOne(req.body)
-                return res.status(200).send('produto atualizado con sucesso!')
+                const findById = {where: {_id: req.params.id}}
+                await productCar.updateOne(findById, req.body)
+                const updateProduct = await productCar.findOne(findById)
+                return res.status(200).send(updateProduct)
             } catch(err) {
                 return res.status(400).send(err)
             } 
@@ -94,7 +98,7 @@ app.post('/product', async (req, res) => {
         });
 
         //Requisição para deletar um carro
-        app.delete('/deletar/:id', async(req, res) => {
+        app.delete('/carros/:id', async(req, res) => {
             try {
                 const getDelete = await productCar.deleteOne({where: {_id: req.params.id}})
                 return res.status(200).send('Produto deletado no banco de dados')
